935e670906f771ff501183dbe5aa26aee352d7e4
Sathvik2021 <91648037+Sathvik2021@users.noreply.github.com>
2022-10-08 13:21:49 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+var first_name="Sathvik";
+var last_name="Chakravarthy";
+console.log(first_name);
+console.log(last_name);
+first_name.concat(last_name);
+var a=10;
+var b=5;
+console.log(a);
+console.log(b)
+var add= a + b;
+console.log(add);
+var sqrt=Math.sqrt(81);
+console.log(sqrt)
+var maximum_number=Math.max(100,10);
+var minimum_number=Math.min(100,10);
